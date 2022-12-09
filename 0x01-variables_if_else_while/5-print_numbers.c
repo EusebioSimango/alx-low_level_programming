8e1854d14aaea39433b432f5268bdc351d20ba30
Eusebio Simango <eusebiosimango14@gmail.com>
2022-12-10 00:22:53 +0200

    Create 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: print the number of base 10.
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf(i);
+	printf("\n");
+	return (0);
+}
+
