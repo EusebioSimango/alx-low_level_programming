5d90f0b5115f607ebdb87ec88f03014730605b02
Eusebio Simango <eusebiosimango14@gmail.com>
2022-12-10 01:54:05 +0200

    Create 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: print only the smallest comb. of 2 digit.
+ * Retuurn: Always 0 (Success)
+*/
+int main(void)
+{
+	int firstDigit;
+	int secondDigit;
+	
+	firstDigit = 48;
+	while (1 == 1)
+	{
+		for (secondDigit = 49; secondDigit < 58; secondDigit++)
+		{
+			if ((firstDigit != secondDigit) && (secondDigit > firstDigit))
+			{
+				putchar(firstDigit);
+				putchar(secondDigit);
+				if ((firstDigit == 56) && (secondDigit == 57))
+					break;
+				putchar(44);
+				putchar(32);
+			}
+		}
+		if (firstDigit == 57)
+			break;
+		firstDigit++;
+	}
+	putchar(10);
+	return (0);
+}
+
