c2f8eaf8df243772221e07a47c9a6babf9c39993
Eusebio Simango <eusebiosimango14@gmail.com>
2022-12-09 23:48:45 +0200

    Create 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: print a lowerCase alphabet,
+ *  followed by uppercase and then a new line 
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int asciiLowerCase;
+	int asciiUpperCase;
+
+	for (asciiLowerCase = 97; asciiLoweCase < 123; asciiLowerCase++)
+		putchar(asciiLowerCase);
+	for (asciiUpperCase = 65; asciiUpperCase < 91; asciiUpperCase++)
+		putchar(asciiUpperCase);
+	putchar(10);
+	return (0);
+}
+
