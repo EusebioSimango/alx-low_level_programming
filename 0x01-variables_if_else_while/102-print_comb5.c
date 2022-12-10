93f25c203dcdebc9c06687db9c2977641ed189ba
Eusebio Simango <eusebiosimango14@gmail.com>
2022-12-10 03:28:55 +0200

    Create 102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: Print comb. of two two-digit numbers.
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i, j;
+	
+	for (i = 0; i < 100; i++)
+	{
+		for (j = 0; j < 100; j++)
+		{
+			if (i < j)
+			{
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
+				putchar(' ');
+				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
+				if (i != 98 || j != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar(10);
+	return (0);
+}
+
